4ac65412a73ecdadbf15f4e99c843297ab6879a6
Pedro <2574151141308@ingenieria.usac.edu.gt>
2020-08-13 22:21:55 -0600

    menu

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# -OLC1-Proyecto1_201700656
+# OLC1_Proyecto1_201700656
 </p>La idea principal del proyecto es que se genere un analisis lexico de cada uno de los distintos archivos de entrada los cuales pueden ser de tipo Marcado como en el caso de HTML, de tipo diseño como CSS o de tipo interpretado como JavaScript. 
 Se debe realizar un analizador léxico por lenguaje ya que en cada uno de ellos existen distintos tipos de caracteres reconocidos. Y como salida se espera la construcción del proyecto en directorios los cuales estarán identificados por medio de paths las cuales serán tomadas y almacenadas por cada archivo dentro de la ejecución del programa, reportando todos los errores léxicos encontrados. 
 Cabe mencionar que al momento de reconocer un error léxico el análisis no debe detenerse por ningún motivo, sino que debe tener la capacidad de recuperarse y seguir analizando. La idea es lograr obtener todos los errores. </p>
--- Window.py
+++ Window.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import tkinter as messagebox
+import tkinter as filedialog
+
 
 #clase ventana
 class Window():
@@ -15,17 +18,34 @@ class Window():
         self.root.title("Ventana Principal")
         self.root.resizable(False,False)
         self.root.config(menu=self.menu)
+        self.initMenu() #metodo que inicia todos los componentes del menu
         self.frame.pack()
         self.frame.config(bg="white")
         self.frame.config(width="900", height="600")
 
-    #metodo que genera el menu del programa
+    #metodo que genera el menu del root window
     def initMenu(self):
-        
-        pass    
-        
-       
+        fileMenu = tk.Menu(self.menu, tearoff=0)
+        fileMenu.add_command(label="Nuevo")
+        fileMenu.add_command(label="Abrir")
+        fileMenu.add_command(label="Guardar")
+        fileMenu.add_command(label="Guardar Como")
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Salir")
+        fileEdit = tk.Menu(self.menu,  tearoff=0)
+        fileEdit.add_command(label="Copiar")
+        fileEdit.add_command(label="Cortar")
+        fileEdit.add_command(label="Pegar")
+        analiticLex = tk.Menu(self.menu, tearoff=0)
+        fileHelp = tk.Menu(self.menu,  tearoff=0)
+        self.menu.add_cascade(label="Archivo", menu=fileMenu)
+        self.menu.add_cascade(label="Editar", menu=fileEdit)
+        self.menu.add_cascade(label="Analizar", menu=analiticLex)
+        self.menu.add_cascade(label="Ayuda", menu=fileHelp) 
 
+    def openFile(self):
+        print("open file dialog")
+        pass
     
     
 
